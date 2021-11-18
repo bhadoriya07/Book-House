abff05f91ab910b1a9b860a490ba01981e6a0de5
unknown <neeleshbhadoriya786@gmail.com>
2021-11-18 21:49:22 +0530

    requirements updated

--- BookHouse/settings.py
+++ BookHouse/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,6 @@ STATICFILES_DIRS=[
 ]
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
