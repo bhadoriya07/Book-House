46f887da45b982c7cad8ddb1f8fa6953c87671fa
bhadoriya07 <90673355+bhadoriya07@users.noreply.github.com>
2023-01-23 12:14:28 +0530

    Update settings.py

--- BookHouse/settings.py
+++ BookHouse/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,4 +140,4 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 #Activate Django-Heroku.
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
